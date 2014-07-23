13b4562881ba6462dbf7345dc1de65c2f92c2ace
Frank Fang <frankfang1990@gmail.com>
2014-07-24 01:14:32 +0800

    add api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+router.use(function (req, res, next) {
+    console.log(req)
+    next();
+});
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Home' });
+});
+router.get('/admin', function (req, res) {
+    res.render('admin', { title: 'Admin' });
 });
 
 module.exports = router;
