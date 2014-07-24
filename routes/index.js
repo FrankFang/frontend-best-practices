716c74809d11e11f74720606debd92e45917231e
Frank Fang <frankfang1990@gmail.com>
2014-07-24 10:03:33 +0800

    remove console logging

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-router.use(function (req, res, next) {
-    console.log(req)
-    next();
-});
 /* GET home page. */
 router.get('/', function (req, res) {
     res.render('index', { title: 'Home' });
