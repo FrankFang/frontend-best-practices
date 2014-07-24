367f8c9803f0b1d99b6d367c77ab9fb7393ec132
Frank Fang <frankfang1990@gmail.com>
2014-07-24 12:57:48 +0800

    update

--- server/server-app.js
+++ server/server-app.js
@@ -13,7 +13,7 @@ var apiRoutes = require('./routes/api');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, '../web/src'));
+app.set('views', path.join(__dirname, '../web/dist'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
@@ -22,14 +22,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../web/src/public')));
+app.use(express.static(path.join(__dirname, '../web/dist/static')));
 
 app.use('/', routes);
 app.use('/admin', adminRoutes);
 app.use('/api', apiRoutes);
 
 /// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -40,7 +40,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -51,7 +51,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
--- web/Gulpfile.js
+++ web/Gulpfile.js
@@ -2,15 +2,46 @@
 /*jshint node:true, strict:false, asi: true*/
 var gulp = require('gulp')
 var jade = require('gulp-jade');
+var chalk = require('chalk')
+var clean = require('gulp-clean')
+var ignore = require('gulp-ignore')
+var changed = require('gulp-changed')
 
 
 var myPaths = {
+    src: './src/',
+    dist: './dist/',
     jade: './src/*.jade',
-    dist: './dist/'
+    copy: './src/static/**',
 }
 
-gulp.task('html', function () {
+gulp.task('clean', function (done) {
+    gulp.src(myPaths.dist + '**/*.*', {read: false})
+        .pipe(clean())
+    done()
+});
+
+gulp.task('jade2html', function () {
     gulp.src(myPaths.jade)
+        .pipe(changed(myPaths.dist))
         .pipe(jade())
         .pipe(gulp.dest(myPaths.dist))
 });
+gulp.task('copy', function () {
+    gulp.src(myPaths.copy, {base: myPaths.src})
+        .pipe(changed(myPaths.dist))
+        .pipe(gulp.dest(myPaths.dist))
+});
+
+gulp.task('watch', function () {
+    gulp.watch([myPaths.copy], ['copy'])
+    gulp.watch([myPaths.jade], ['jade2html'])
+});
+
+gulp.task('debug', ['clean', 'copy', 'jade2html', 'watch'], function () {
+    console.info(chalk.black.bgWhite.bold('You can debug now!'))
+})
+
+gulp.task('build', [], function () {
+    console.info(chalk.black.bgWhite.bold('Building tasks done!'))
+})
