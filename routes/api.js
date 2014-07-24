3a3ef993a1609b7522a1cb97c946db064e52c68a
Frank Fang <frankfang1990@gmail.com>
2014-07-24 10:25:15 +0800

    api ok

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,14 @@
+/*jshint node:true, strict:false, asi: true*/
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function (req, res) {
-    res.send('API list');
-});
+router.get('/1/login', function (req, res) {
+    var status = req.query.status
+    res.set('Content-Type', 'application/json')
+    res.json({
+        ret: status || 0
+    })
+})
 
-module.exports = router;
+module.exports = router
