d82e53e5231dcd99c1713362c115d278e6cc56b5
Frank Fang <frankfang1990@gmail.com>
2014-07-29 11:58:27 +0800

    fix server problem

--- server2/routes/admin.js
+++
@@ -1,9 +0,0 @@
-/* Created by frank on 14-7-24. */
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function (req, res) {
-    res.render('admin', { title: 'Admin' });
-});
-
-module.exports = router;
